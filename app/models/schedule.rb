e42d29c6c1604b3c8d089b811ae0899ee740b057
yamagishi <spica_sigsig@yahoo.co.jp>
2022-11-20 10:54:15 +0000

    バリデーションのエラー修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -8,14 +8,14 @@ class Schedule < ApplicationRecord
   validate :start_end_check
    
   def date_before_start
-    errors.add(:start, "は今日以降のものを選択してください") if start < Date.today
+    return if start.blank?
+    errors.add(:start, "は今日以降のものを選択してください") if start < Date.today 
   end
   
   def start_end_check
+    return if self.end.blank? || self.start.blank?
     errors.add(:end, "は開始日より前の日付は登録できません") unless
     self.start < self.end 
   end
-
-
- 
+  
 end
